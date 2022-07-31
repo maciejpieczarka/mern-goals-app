638477091d21eb034649bb0fb68005bf18f31453
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2022-07-31 14:48:36 +0200

    Add protection for goal operations

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -8,10 +8,12 @@ const {
   updateGoal,
 } = require("./../controllers/goalController");
 
+const { protect } = require("./../middleware/authMiddleware");
+
 //Route for getting and setting goals
-router.route("/").get(getGoals).post(setGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
 
 //Route for updating and removing goals
-router.route("/:id").put(updateGoal).delete(removeGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, removeGoal);
 
 module.exports = router;
